e7325444fe247f24d6ccc73a27b2770ffb317900
Untoldwind <landru@untoldwind.net>
2013-08-23 00:02:40 +0200

    Some more tests

--- core/src/test/scala/de/leanovate/jbj/tests/TestBed.scala
+++ core/src/test/scala/de/leanovate/jbj/tests/TestBed.scala
@@ -45,63 +45,36 @@ object TestBed {
   def main(args: Array[String]) {
     test(
       """<?php
-        |error_reporting(E_ALL & ~E_STRICT);
-        |
-        |function f() { return 0; }
-        |$a[0][1] = 'good';
-        |$a[1][1] = 'bad';
-        |
-        |echo "\n" . '$i=f(): ';
-        |echo $a[$i=f()][++$i];
-        |unset($i);
-        |
-        |echo "\n" . '$$x=f(): ';
-        |$x='i';
-        |echo $a[$$x=f()][++$$x];
-        |unset($i, $x);
-        |
-        |echo "\n" . '${\'i\'}=f(): ';
-        |echo $a[${'i'}=f()][++${'i'}];
-        |unset(${'i'});
-        |
-        |echo "\n" . '$i[0]=f(): ';
-        |echo $a[$i[0]=f()][++$i[0]];
-        |unset($i);
-        |
-        |echo "\n" . '$i[0][0]=f(): ';
-        |echo $a[$i[0][0]=f()][++$i[0][0]];
-        |unset($i);
-        |
-        |echo "\n" . '$i->p=f(): ';
-        |echo $a[$i->p=f()][++$i->p];
-        |unset($i);
-        |
-        |echo "\n" . '$i->p->q=f(): ';
-        |echo $a[$i->p->q=f()][++$i->p->q];
-        |unset($i);
-        |
-        |echo "\n" . '$i->p[0]=f(): ';
-        |echo $a[$i->p[0]=f()][++$i->p[0]];
-        |unset($i);
-        |
-        |echo "\n" . '$i->p[0]->p=f(): ';
-        |echo $a[$i->p[0]->p=f()][++$i->p[0]->p];
-        |unset($i);
         |
         |Class C {
-        |	static $p;
+        |	function f() {
+        |		static $a = array(1,2,3);
+        |		eval(' static $k = array(4,5,6); ');
+        |
+        |		function cfg() {
+        |			static $a = array(7,8,9);
+        |			eval(' static $k = array(10,11,12); ');
+        |			var_dump($a, $k);
+        |		}
+        |		var_dump($a, $k);
+        |	}
         |}
+        |$c = new C;
+        |$c->f();
+        |cfg();
         |
-        |echo "\n" . 'C::$p=f(): ';
-        |echo $a[C::$p=f()][++C::$p];
+        |Class D {
+        |	static function f() {
+        |		eval('function dfg() { static $b = array(1,2,3); var_dump($b); } ');
+        |	}
+        |}
+        |D::f();
+        |dfg();
         |
-        |echo "\n" . 'C::$p[0]=f(): ';
-        |C::$p = array();
-        |echo $a[C::$p[0]=f()][++C::$p[0]];
+        |eval(' Class E { function f() { static $c = array(1,2,3); var_dump($c); } }');
+        |$e = new E;
+        |$e->f();
         |
-        |echo "\n" . 'C::$p->q=f(): ';
-        |C::$p = new stdclass;
-        |echo $a[C::$p->q=f()][++C::$p->q];
         |?>""".stripMargin)
   }
 }
