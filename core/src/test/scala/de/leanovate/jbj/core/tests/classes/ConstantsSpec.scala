682a763770e37c3a91583766a4915b6472b45e71
Untoldwind <landru@untoldwind.net>
2013-08-29 21:31:33 +0200

    Additional tests

--- core/src/test/scala/de/leanovate/jbj/core/tests/classes/ConstantsSpec.scala
+++ core/src/test/scala/de/leanovate/jbj/core/tests/classes/ConstantsSpec.scala
@@ -272,5 +272,73 @@ class ConstantsSpec extends SpecificationWithJUnit with TestJbjExecutor {
           |""".stripMargin
       )
     }
+
+    "Test constants with default values based on other constants." in {
+      // classes/constants_basic_005.phpt
+      script(
+        """<?php
+          |  class C
+          |  {
+          |      const CONST_2 = self::CONST_1;
+          |      const CONST_1 = self::BASE_CONST;
+          |      const BASE_CONST = 'hello';
+          |  }
+          |  var_dump(C::CONST_1, C::CONST_2);
+          |?>
+          |""".stripMargin
+      ).result must haveOutput(
+        """string(5) "hello"
+          |string(5) "hello"
+          |""".stripMargin
+      )
+    }
+
+    "Ensure class constants are not evaluated when a class is looked up to resolve inheritance during runtime." in {
+      // classes/constants_basic_006.phpt
+      script(
+        """<?php
+          |  class C
+          |  {
+          |      const X = E::A;
+          |      public static $a = array(K => D::V, E::A => K);
+          |  }
+          |
+          |  eval('class D extends C { const V = \'test\'; }');
+          |
+          |  class E extends D
+          |  {
+          |      const A = "hello";
+          |  }
+          |
+          |  define('K', "nasty");
+          |
+          |  var_dump(C::X, C::$a, D::X, D::$a, E::X, E::$a);
+          |?>
+          |""".stripMargin
+      ).result must haveOutput(
+        """string(5) "hello"
+          |array(2) {
+          |  ["nasty"]=>
+          |  string(4) "test"
+          |  ["hello"]=>
+          |  string(5) "nasty"
+          |}
+          |string(5) "hello"
+          |array(2) {
+          |  ["nasty"]=>
+          |  string(4) "test"
+          |  ["hello"]=>
+          |  string(5) "nasty"
+          |}
+          |string(5) "hello"
+          |array(2) {
+          |  ["nasty"]=>
+          |  string(4) "test"
+          |  ["hello"]=>
+          |  string(5) "nasty"
+          |}
+          |""".stripMargin
+      )
+    }
   }
 }
