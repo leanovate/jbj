f4c04337290c801419806a746cddb53dd21fb6f1
Untoldwind <landru@untoldwind.net>
2013-09-03 10:52:23 +0200

    Some more tests

--- core/src/test/scala/de/leanovate/jbj/core/tests/TestBed.scala
+++ core/src/test/scala/de/leanovate/jbj/core/tests/TestBed.scala
@@ -53,10 +53,24 @@ object TestBed {
   def main(args: Array[String]) {
     test(
       """<?php
-        |$a = array("a","b","c");
-        |foreach ($a as &$k=>$v) {
-        |  var_dump($v);
+        |echo "\nChange from array to non iterable:\n";
+        |$a = array(1,2,3);
+        |$b=&$a;
+        |foreach ($a as $v) {
+        |	var_dump($v);
+        |	$b=1;
         |}
+        |
+        |echo "\nChange from object to non iterable:\n";
+        |$a = new stdClass;
+        |$a->a=1;
+        |$a->b=2;
+        |$b=&$a;
+        |foreach ($a as $v) {
+        |	var_dump($v);
+        |	$b='x';
+        |}
+        |
         |?>""".stripMargin)
   }
 }
