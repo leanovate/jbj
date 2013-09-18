7efe5da1d44d1c49892f05ebca1289edc5304ac2
Untoldwind <landru@untoldwind.net>
2013-09-18 22:51:07 +0200

    Some more tests

--- core/src/test/scala/de/leanovate/jbj/core/tests/lang/Bug25xxxSpec.scala
+++ core/src/test/scala/de/leanovate/jbj/core/tests/lang/Bug25xxxSpec.scala
@@ -12,6 +12,29 @@ import de.leanovate.jbj.core.tests.TestJbjExecutor
 
 class Bug25xxxSpec extends SpecificationWithJUnit with TestJbjExecutor {
   "Bugs #25xxx" should {
+    """Bug #25145 (SEGV on recpt of form input with name like "123[]")""" in {
+      // lang/bug25145.phpt
+      script(
+        """<?php
+          |
+          |var_dump($_REQUEST);
+          |echo "Done\n";
+          |
+          |?>
+          |""".stripMargin
+      ).withGet("?123[]=SEGV").result must haveOutput(
+        """array(1) {
+          |  [123]=>
+          |  array(1) {
+          |    [0]=>
+          |    string(4) "SEGV"
+          |  }
+          |}
+          |Done
+          |""".stripMargin
+      )
+    }
+
     "Bug #25547 (error_handler and array index with function call)" in {
       // lang/bug25547.phpt
       script(
@@ -43,6 +66,33 @@ class Bug25xxxSpec extends SpecificationWithJUnit with TestJbjExecutor {
           |    [foo] => 1
           |)
           |Done""".stripMargin
+      )
+    }
+
+    "Bug #25652 (Calling Global functions dynamically fails from Class scope)" in {
+      // lang/bug25652.phpt
+      script(
+        """<?php
+          |
+          |	function testfunc ($var) {
+          |		echo "testfunc $var\n";
+          |	}
+          |
+          |	class foo {
+          |		public $arr = array('testfunc');
+          |		function bar () {
+          |			$this->arr[0]('testvalue');
+          |		}
+          |	}
+          |
+          |	$a = new foo ();
+          |	$a->bar ();
+          |
+          |?>
+          |""".stripMargin
+      ).result must haveOutput(
+        """testfunc testvalue
+          |""".stripMargin
       )
     }
   }
