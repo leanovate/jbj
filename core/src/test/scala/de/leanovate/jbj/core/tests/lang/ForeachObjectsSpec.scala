7146c584aa63190ec4053fdb89c5a77c1302c8e2
Untoldwind <landru@untoldwind.net>
2013-09-27 13:59:25 +0200

    More tests

--- core/src/test/scala/de/leanovate/jbj/core/tests/lang/ForeachObjectsSpec.scala
+++ core/src/test/scala/de/leanovate/jbj/core/tests/lang/ForeachObjectsSpec.scala
@@ -1684,5 +1684,301 @@ class ForeachObjectsSpec extends SpecificationWithJUnit with TestJbjExecutor {
           |""".stripMargin
       )
     }
+
+    "Foreach loop tests - Removing the current element from an iterated object." in {
+      // lang/foreachLoopObjects.004.phpt
+      script(
+        """<?php
+          |
+          |class C {
+          |	public $a = "Original a";
+          |	public $b = "Original b";
+          |	public $c = "Original c";
+          |	public $d = "Original d";
+          |	public $e = "Original e";
+          |}
+          |
+          |echo "\nRemoving the current element from an iterated object.\n";
+          |$obj = new C;
+          |$count=0;
+          |foreach ($obj as $v) {
+          |	if ($v==$obj->b) {
+          |		unset($obj->b);
+          |	}
+          |	var_dump($v);
+          |	if (++$count>10) {
+          |		echo "Loop detected.\n";
+          |		break;
+          |	}
+          |}
+          |var_dump($obj);
+          |
+          |
+          |?>
+          |===DONE===
+          |""".stripMargin
+      ).result must haveOutput(
+        """
+          |Removing the current element from an iterated object.
+          |string(10) "Original a"
+          |string(10) "Original b"
+          |
+          |Notice: Undefined property: C::$b in /lang/ForeachObjectsSpec.inlinePhp on line 15
+          |string(10) "Original c"
+          |
+          |Notice: Undefined property: C::$b in /lang/ForeachObjectsSpec.inlinePhp on line 15
+          |string(10) "Original d"
+          |
+          |Notice: Undefined property: C::$b in /lang/ForeachObjectsSpec.inlinePhp on line 15
+          |string(10) "Original e"
+          |object(C)#1 (4) {
+          |  ["a"]=>
+          |  string(10) "Original a"
+          |  ["c"]=>
+          |  string(10) "Original c"
+          |  ["d"]=>
+          |  string(10) "Original d"
+          |  ["e"]=>
+          |  string(10) "Original e"
+          |}
+          |===DONE===
+          |""".stripMargin
+      )
+    }
+
+    "Foreach loop tests - removing properties before and after the current property during the loop." in {
+      // lang/foreachLoopObjects.005.phpt
+      script(
+        """<?php
+          |
+          |class C {
+          |	public $a = "Original a";
+          |	public $b = "Original b";
+          |	public $c = "Original c";
+          |	public $d = "Original d";
+          |	public $e = "Original e";
+          |}
+          |
+          |echo "\nRemoving properties before the current element from an iterated object.\n";
+          |$obj = new C;
+          |$count=0;
+          |foreach ($obj as $v) {
+          |	if ($v==$obj->a) {
+          |		unset($obj->c);
+          |	}
+          |	var_dump($v);
+          |	if (++$count>10) {
+          |		echo "Loop detected.\n";
+          |		break;
+          |	}
+          |}
+          |var_dump($obj);
+          |
+          |echo "\nRemoving properties before the current element from an iterated object.\n";
+          |$obj = new C;
+          |foreach ($obj as $v) {
+          |	if ($v==$obj->b) {
+          |		unset($obj->a);
+          |	}
+          |	var_dump($v);
+          |	if (++$count>10) {
+          |		echo "Loop detected.\n";
+          |		break;
+          |	}
+          |}
+          |var_dump($obj);
+          |
+          |
+          |?>
+          |""".stripMargin
+      ).result must haveOutput(
+        """
+          |Removing properties before the current element from an iterated object.
+          |string(10) "Original a"
+          |string(10) "Original b"
+          |string(10) "Original d"
+          |string(10) "Original e"
+          |object(C)#1 (4) {
+          |  ["a"]=>
+          |  string(10) "Original a"
+          |  ["b"]=>
+          |  string(10) "Original b"
+          |  ["d"]=>
+          |  string(10) "Original d"
+          |  ["e"]=>
+          |  string(10) "Original e"
+          |}
+          |
+          |Removing properties before the current element from an iterated object.
+          |string(10) "Original a"
+          |string(10) "Original b"
+          |string(10) "Original c"
+          |string(10) "Original d"
+          |string(10) "Original e"
+          |object(C)#2 (4) {
+          |  ["b"]=>
+          |  string(10) "Original b"
+          |  ["c"]=>
+          |  string(10) "Original c"
+          |  ["d"]=>
+          |  string(10) "Original d"
+          |  ["e"]=>
+          |  string(10) "Original e"
+          |}
+          |""".stripMargin
+      )
+    }
+
+    "Foreach loop tests - substituting the entire iterated entity during the loop." in {
+      // lang/foreachLoopObjects.006.phpt
+      script(
+        """<?php
+          |
+          |class C {
+          |	public $a = "Original a";
+          |	public $b = "Original b";
+          |	public $c = "Original c";
+          |	public $d = "Original d";
+          |	public $e = "Original e";
+          |}
+          |
+          |echo "\nSubstituting the iterated object for a different object.\n";
+          |$obj = new C;
+          |$obj2 = new stdclass;
+          |$obj2->a = "new a";
+          |$obj2->b = "new b";
+          |$obj2->c = "new c";
+          |$obj2->d = "new d";
+          |$obj2->e = "new e";
+          |$obj2->f = "new f";
+          |$ref = &$obj;
+          |$count=0;
+          |foreach ($obj as $v) {
+          |	var_dump($v);
+          |	if ($v==$obj->b) {
+          |	  $ref=$obj2;
+          |	}
+          |	if (++$count>10) {
+          |		echo "Loop detected.\n";
+          |		break;
+          |	}
+          |}
+          |var_dump($obj);
+          |
+          |echo "\nSubstituting the iterated object for an array.\n";
+          |$obj = new C;
+          |$a = array(1,2,3,4,5,6,7,8);
+          |$ref = &$obj;
+          |$count=0;
+          |foreach ($obj as $v) {
+          |	var_dump($v);
+          |	if ($v==="Original b") {
+          |	  $ref=$a;
+          |	}
+          |	if (++$count>10) {
+          |		echo "Loop detected.\n";
+          |		break;
+          |	}
+          |}
+          |var_dump($obj);
+          |
+          |echo "\nSubstituting the iterated array for an object.\n";
+          |$a = array(1,2,3,4,5,6,7,8);
+          |$obj = new C;
+          |$ref = &$a;
+          |$count=0;
+          |foreach ($a as $v) {
+          |	var_dump($v);
+          |	if ($v===2) {
+          |	  $ref=$obj;
+          |	}
+          |	if (++$count>10) {
+          |		echo "Loop detected.\n";
+          |		break;
+          |	}
+          |}
+          |var_dump($obj);
+          |
+          |?>
+          |""".stripMargin
+      ).result must haveOutput(
+        """
+          |Substituting the iterated object for a different object.
+          |string(10) "Original a"
+          |string(10) "Original b"
+          |string(5) "new a"
+          |string(5) "new b"
+          |string(5) "new c"
+          |string(5) "new d"
+          |string(5) "new e"
+          |string(5) "new f"
+          |object(stdClass)#2 (6) {
+          |  ["a"]=>
+          |  string(5) "new a"
+          |  ["b"]=>
+          |  string(5) "new b"
+          |  ["c"]=>
+          |  string(5) "new c"
+          |  ["d"]=>
+          |  string(5) "new d"
+          |  ["e"]=>
+          |  string(5) "new e"
+          |  ["f"]=>
+          |  string(5) "new f"
+          |}
+          |
+          |Substituting the iterated object for an array.
+          |string(10) "Original a"
+          |string(10) "Original b"
+          |int(1)
+          |int(2)
+          |int(3)
+          |int(4)
+          |int(5)
+          |int(6)
+          |int(7)
+          |int(8)
+          |array(8) {
+          |  [0]=>
+          |  int(1)
+          |  [1]=>
+          |  int(2)
+          |  [2]=>
+          |  int(3)
+          |  [3]=>
+          |  int(4)
+          |  [4]=>
+          |  int(5)
+          |  [5]=>
+          |  int(6)
+          |  [6]=>
+          |  int(7)
+          |  [7]=>
+          |  int(8)
+          |}
+          |
+          |Substituting the iterated array for an object.
+          |int(1)
+          |int(2)
+          |string(10) "Original a"
+          |string(10) "Original b"
+          |string(10) "Original c"
+          |string(10) "Original d"
+          |string(10) "Original e"
+          |object(C)#4 (5) {
+          |  ["a"]=>
+          |  string(10) "Original a"
+          |  ["b"]=>
+          |  string(10) "Original b"
+          |  ["c"]=>
+          |  string(10) "Original c"
+          |  ["d"]=>
+          |  string(10) "Original d"
+          |  ["e"]=>
+          |  string(10) "Original e"
+          |}
+          |""".stripMargin
+      )
+    }
   }
 }
