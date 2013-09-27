91309f4c495834daa121bae39e13f55110f9993f
Untoldwind <landru@untoldwind.net>
2013-09-27 22:32:19 +0200

    More tests

--- core/src/test/scala/de/leanovate/jbj/core/tests/zend/Namespace4Spec.scala
+++ core/src/test/scala/de/leanovate/jbj/core/tests/zend/Namespace4Spec.scala
@@ -6,7 +6,7 @@ import de.leanovate.jbj.core.tests.TestJbjExecutor
 class Namespace4Spec extends SpecificationWithJUnit with TestJbjExecutor {
   "Namespace test 030-039" should {
     "030: Name ambiguity (import name & class name)" in {
-      // ../php-src/Zend/tests/ns_030.phpt
+      // Zend/tests/ns_030.phpt
       script(
         """<?php
           |class Foo {
@@ -19,6 +19,56 @@ class Namespace4Spec extends SpecificationWithJUnit with TestJbjExecutor {
       ).result must haveOutput(
         """
           |Fatal error: Cannot use A\B as Foo because the name is already in use in /zend/Namespace4Spec.inlinePhp on line 5
+          |""".stripMargin
+      )
+    }
+
+    "031: Namespace support for user functions (ns name)" in {
+      // Zend/tests/ns_031.phpt
+      script(
+        """<?php
+          |namespace test;
+          |
+          |class Test {
+          |	static function foo() {
+          |		echo __CLASS__,"::",__FUNCTION__,"\n";
+          |	}
+          |}
+          |
+          |function foo() {
+          |	echo __FUNCTION__,"\n";
+          |}
+          |
+          |call_user_func(__NAMESPACE__."\\foo");
+          |call_user_func(__NAMESPACE__."\\test::foo");
+          |""".stripMargin
+      ).result must haveOutput(
+        """test\foo
+          |test\Test::foo
+          |""".stripMargin
+      )
+    }
+
+    "032: Namespace support for user functions (php name)" in {
+      // Zend/tests/ns_032.phpt
+      script(
+        """<?php
+          |class Test {
+          |	static function foo() {
+          |		echo __CLASS__,"::",__FUNCTION__,"\n";
+          |	}
+          |}
+          |
+          |function foo() {
+          |	echo __FUNCTION__,"\n";
+          |}
+          |
+          |call_user_func(__NAMESPACE__."\\foo");
+          |call_user_func(__NAMESPACE__."\\test::foo");
+          |""".stripMargin
+      ).result must haveOutput(
+        """foo
+          |Test::foo
           |""".stripMargin
       )
     }
