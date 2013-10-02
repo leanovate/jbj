3ebc7f16995974eb8cf6258638f9f82e91e85ea9
Untoldwind <landru@untoldwind.net>
2013-10-02 21:57:55 +0200

    Check for array in const

--- core/src/main/scala/de/leanovate/jbj/core/ast/stmt/ConstDeclStmt.scala
+++ core/src/main/scala/de/leanovate/jbj/core/ast/stmt/ConstDeclStmt.scala
@@ -10,15 +10,22 @@ package de.leanovate.jbj.core.ast.stmt
 import de.leanovate.jbj.core.ast.Stmt
 import de.leanovate.jbj.runtime.context.Context
 import de.leanovate.jbj.runtime.{NamespaceName, SuccessExecResult}
-import de.leanovate.jbj.runtime.value.LazyVal
+import de.leanovate.jbj.runtime.value.{ArrayVal, LazyVal}
+import de.leanovate.jbj.runtime.exception.FatalErrorJbjException
+import de.leanovate.jbj.core.ast.expr.ArrayCreateExpr
 
 case class ConstDeclStmt(assignments: List[StaticAssignment]) extends Stmt{
   override def exec(implicit ctx: Context) = {
     assignments.foreach {
       case StaticAssignment(name, Some(expr)) =>
-        ctx.global.defineConstant(NamespaceName(name).absolutePrefix, new LazyVal {
-          def value = expr.eval.concrete
-        }, caseInsensitive = false)
+        expr match {
+          case _: ArrayCreateExpr =>
+            throw new FatalErrorJbjException("Arrays are not allowed as constants")
+          case _ =>
+            ctx.global.defineConstant(NamespaceName(name).absolutePrefix, new LazyVal {
+              def value = expr.eval.concrete
+            }, caseInsensitive = false)
+        }
       case _ =>
     }
     SuccessExecResult
--- core/src/test/scala/de/leanovate/jbj/core/tests/zend/Namespace6Spec.scala
+++ core/src/test/scala/de/leanovate/jbj/core/tests/zend/Namespace6Spec.scala
@@ -241,5 +241,80 @@ class Namespace6Spec extends SpecificationWithJUnit with TestJbjExecutor {
           |""".stripMargin
       )
     }
+
+    "058: Usage of 'namespace' in compound names (out of namespase)" in {
+      // Zend/tests/ns_058.phpt
+      script(
+        """<?php
+          |const C = "const ok\n";
+          |
+          |function foo() {
+          |	return "func ok\n";
+          |}
+          |
+          |class foo {
+          |	const C = "const ok\n";
+          |	const C2 = namespace\C;
+          |	static $var = "var ok\n";
+          |	function __construct() {
+          |		echo "class ok\n";
+          |	}
+          |	static function bar() {
+          |		return "method ok\n";
+          |	}
+          |}
+          |
+          |function f1($x=namespace\C) {
+          |	return $x;
+          |}
+          |function f2($x=namespace\foo::C) {
+          |	return $x;
+          |}
+          |
+          |function f3(namespace\foo $x) {
+          |	return "ok\n";
+          |}
+          |
+          |echo namespace\C;
+          |echo namespace\foo();
+          |echo namespace\foo::C;
+          |echo namespace\foo::C2;
+          |echo namespace\foo::$var;
+          |echo namespace\foo::bar();
+          |echo namespace\f1();
+          |echo namespace\f2();
+          |echo namespace\f3(new namespace\foo());
+          |echo namespace\nknown;
+          |?>
+          |""".stripMargin
+      ).result must haveOutput(
+        """const ok
+          |func ok
+          |const ok
+          |const ok
+          |var ok
+          |method ok
+          |const ok
+          |const ok
+          |class ok
+          |ok
+          |
+          |Fatal error: Undefined constant 'nknown' in /zend/Namespace6Spec.inlinePhp on line 40
+          |""".stripMargin
+      )
+    }
+
+    "059: Constant arrays" in {
+      // Zend/tests/ns_059.phpt
+      script(
+        """<?php
+          |const C = array();
+          |""".stripMargin
+      ).result must haveOutput(
+        """
+          |Fatal error: Arrays are not allowed as constants in /zend/Namespace6Spec.inlinePhp on line 2
+          |""".stripMargin
+      )
+    }
   }
 }
