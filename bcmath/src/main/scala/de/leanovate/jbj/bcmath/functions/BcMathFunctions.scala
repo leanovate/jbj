437dc1f209872aff37f65c0b2290932d925c090d
Yann Simon <yann.simon.fr@gmail.com>
2013-12-18 15:25:52 +0100

    add bccomp and bcdiv

--- bcmath/src/main/scala/de/leanovate/jbj/bcmath/functions/BcMathFunctions.scala
+++ bcmath/src/main/scala/de/leanovate/jbj/bcmath/functions/BcMathFunctions.scala
@@ -2,13 +2,32 @@ package de.leanovate.jbj.bcmath.functions
 
 import de.leanovate.jbj.runtime.annotations.{ParameterMode, GlobalFunction}
 import de.leanovate.jbj.runtime.context.Context
-import scala.math.BigDecimal.RoundingMode
+import scala.math.BigDecimal.RoundingMode.FLOOR
 
 object BcMathFunctions {
+
   @GlobalFunction(parameterMode = ParameterMode.EXACTLY_WARN)
   def bcadd(left: String, right: String, scale: Option[Int])(implicit context: Context): String = {
-    val bcScale = scale.getOrElse(context.settings.getBcScaleFactor)
+    val s = scale.getOrElse(context.settings.getBcScaleFactor)
     val result = BigDecimal(left) + BigDecimal(right)
-    result.setScale(bcScale, RoundingMode.FLOOR).toString()
+    result.setScale(s, FLOOR).toString()
   }
+
+  @GlobalFunction(parameterMode = ParameterMode.EXACTLY_WARN)
+  def bccomp(left: String, right: String, scale: Option[Int])(implicit context: Context): Int = {
+    val s = scale.getOrElse(context.settings.getBcScaleFactor)
+    BigDecimal(left).setScale(s, FLOOR).compare(BigDecimal(right).setScale(s, FLOOR))
+  }
+
+  @GlobalFunction(parameterMode = ParameterMode.STRICT_WARN)
+  def bcdiv(left: String, right: String, scale: Option[Int])(implicit context: Context): String = {
+    val s = scale.getOrElse(context.settings.getBcScaleFactor)
+    try {
+      val result = BigDecimal(left) / BigDecimal(right)
+      result.setScale(s, FLOOR).toString()
+    } catch {
+      case a: java.lang.ArithmeticException => context.log.warn("bcdiv(): " + a.getMessage); ""
+    }
+  }
+
 }
