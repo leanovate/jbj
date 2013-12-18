9e0325b0957dbc2dbf34ee0900be025afd4daca1
Yann Simon <yann.simon.fr@gmail.com>
2013-12-18 16:46:21 +0100

    add bcmod

--- bcmath/src/main/scala/de/leanovate/jbj/bcmath/functions/BcMathFunctions.scala
+++ bcmath/src/main/scala/de/leanovate/jbj/bcmath/functions/BcMathFunctions.scala
@@ -19,15 +19,25 @@ object BcMathFunctions {
     BigDecimal(left).setScale(s, FLOOR).compare(BigDecimal(right).setScale(s, FLOOR))
   }
 
+  private def arithmeticExceptionHandler(functionName: String)(implicit context: Context): PartialFunction[Throwable, String] = {
+    case a: java.lang.ArithmeticException => context.log.warn(functionName + "(): " + a.getMessage); ""
+  }
+
   @GlobalFunction(parameterMode = ParameterMode.STRICT_WARN)
   def bcdiv(left: String, right: String, scale: Option[Int])(implicit context: Context): String = {
     val s = scale.getOrElse(context.settings.getBcScaleFactor)
     try {
       val result = BigDecimal(left) / BigDecimal(right)
       result.setScale(s, FLOOR).toString()
-    } catch {
-      case a: java.lang.ArithmeticException => context.log.warn("bcdiv(): " + a.getMessage); ""
-    }
+    } catch arithmeticExceptionHandler("bcdiv")
+  }
+
+  @GlobalFunction(parameterMode = ParameterMode.EXACTLY_WARN)
+  def bcmod(left: String, right: String)(implicit context: Context): String = {
+    try {
+      val result = BigDecimal(left) % BigDecimal(right)
+      result.toString()
+    } catch arithmeticExceptionHandler("bcmod")
   }
 
 }
