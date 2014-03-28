bb61f1fc9577367db4da3b4ceeeb36382ea4133c
Untoldwind <landru@untoldwind.net>
2014-03-28 12:42:49 +0100

    Add lots of string functions

--- buildins/src/main/scala/de/leanovate/jbj/buildins/functions/StringFunctions.scala
+++ buildins/src/main/scala/de/leanovate/jbj/buildins/functions/StringFunctions.scala
@@ -9,13 +9,34 @@ package de.leanovate.jbj.buildins.functions
 
 import de.leanovate.jbj.runtime.value._
 import de.leanovate.jbj.runtime.annotations.{ParameterMode, GlobalFunction}
-import de.leanovate.jbj.runtime.value.IntegerVal
 import de.leanovate.jbj.runtime.context.Context
-import java.net.URLEncoder
+import java.net.{URLDecoder, URLEncoder}
 import de.leanovate.jbj.runtime.types.PParam
 import de.leanovate.jbj.runtime.adapter.GlobalFunctions
+import java.security.SecureRandom
+import de.leanovate.jbj.runtime.annotations.GlobalFunction
+import scala.Some
+import de.leanovate.jbj.runtime.value.IntegerVal
 
 trait StringFunctions {
+  @GlobalFunction
+  def addslashes(str: String): String = {
+    val sb = new StringBuilder
+    str.foreach {
+      case '\0' =>
+        sb.append("\\0")
+      case '\'' =>
+        sb.append("\\'")
+      case '"' =>
+        sb.append("\\\"")
+      case '\\' =>
+        sb.append("\\\\")
+      case ch =>
+        sb.append(ch)
+    }
+    sb.toString()
+  }
+
   @GlobalFunction
   def bin2hex(str: Array[Byte]): Array[Byte] = {
     val result = new Array[Byte](str.length * 2)
@@ -25,6 +46,27 @@ trait StringFunctions {
       result(i * 2 + 1) = Character.forDigit(ch & 0xf, 16).toByte
     }
     result
+  }
+
+  @GlobalFunction
+  def chr(code: Int): String = {
+    val sb = new StringBuilder
+    sb.append(code.toChar)
+    sb.toString()
+  }
+
+  @GlobalFunction
+  def quotemeta(str: String): String = {
+    val sb = new StringBuilder
+    str.foreach {
+      case ch if ch == '.' | ch == '.' | ch == '\\' | ch == '+' | ch == '*' | ch == '?' |
+        ch == '[' | ch == '^' | ch == ']' | ch == '$' | ch == '(' | ch == ')' =>
+        sb.append('\\')
+        sb.append(ch)
+      case ch =>
+        sb.append(ch)
+    }
+    sb.toString()
   }
 
   @GlobalFunction(parameterMode = ParameterMode.STRICT_WARN, warnResult = NullVal)
@@ -113,6 +155,24 @@ trait StringFunctions {
   }
 
   @GlobalFunction
+  def strchr(haystack: Array[Byte], needle: PVal, beforeNeedle: Option[Boolean]): PVal =
+    strstr(haystack, needle, beforeNeedle)
+
+  @GlobalFunction
+  def strrchr(haystack: Array[Byte], needle: PVal): PVal = {
+    val needleBytes = needle match {
+      case str: StringVal => str.chars
+      case int: IntegerVal => Array(int.asLong.toByte)
+    }
+    val idx = haystack.lastIndexOfSlice(needleBytes)
+    if (idx < 0) {
+      BooleanVal.FALSE
+    } else {
+      StringVal(haystack.drop(idx))
+    }
+  }
+
+  @GlobalFunction
   def strstr(haystack: Array[Byte], needle: PVal, beforeNeedle: Option[Boolean]): PVal = {
     val needleBytes = needle match {
       case str: StringVal => str.chars
@@ -149,6 +209,67 @@ trait StringFunctions {
   def strtoupper(str: String): String = str.toUpperCase
 
   @GlobalFunction
+  def strtok(stringOrToken: String, optToken: Option[String])(implicit ctx: Context): PVal = {
+    val (optRemain, pattern) = optToken.map {
+      token =>
+        Some(stringOrToken) -> token
+    }.getOrElse {
+      val remain = ctx.global.findVariable("__strtok_remain").map(_.asVal.toStr.asUtf8String)
+      remain -> stringOrToken
+    }
+    optRemain.map {
+      remain =>
+        val idx = remain.indexWhere(pattern.contains(_))
+        if (idx >= 0) {
+          ctx.global.findOrDefineVariable("__strtok_remain") := StringVal(remain.substring(idx + 1))
+          StringVal(remain.substring(0, idx))
+        } else {
+          ctx.global.undefineVariable("__strtok_remain")
+          StringVal(remain)
+        }
+    }.getOrElse {
+      BooleanVal.FALSE
+    }
+  }
+
+  @GlobalFunction
+  def substr(str: String, start: Int, optLength: Option[Int])(implicit ctx: Context): PVal = {
+    val realStart = if (start >= 0) start else if (-start > str.length) 0 else str.length + start
+    optLength.map {
+      case length if length < 0 =>
+        val end = str.length + length
+        if (end < realStart)
+          BooleanVal.FALSE
+        else
+          StringVal(str.substring(realStart, end))
+      case length =>
+        val end = if (realStart + length > str.length) str.length else realStart + length
+        StringVal(str.substring(realStart, end))
+    }.getOrElse {
+      if (realStart >= str.length)
+        BooleanVal.FALSE
+      else
+        StringVal(str.substring(start))
+    }
+  }
+
+  @GlobalFunction
+  def strtr(str: String, param1: PVal, param2: Option[String])(implicit ctx: Context): String = {
+    (param1, param2) match {
+      case (ArrayVal(replace_pairs), None) =>
+        replace_pairs.foldLeft(str) {
+          case (prev, (StringVal(from), StringVal(to))) =>
+            prev.replace(from, to)
+        }
+      case (StringVal(from), Some(to)) =>
+        Range(0, math.min(from.length, to.length)).foldLeft(str) {
+          (prev, idx) =>
+            prev.replace(from.charAt(idx), to.charAt(idx))
+        }
+    }
+  }
+
+  @GlobalFunction
   def trim(pVal: PVal)(implicit ctx: Context): PVal = {
     pVal.concrete match {
       case StringVal(str) =>
@@ -170,9 +291,93 @@ trait StringFunctions {
     }
   }
 
+  private val hexdigits = "0123456789ABCDEF".toCharArray
+
   @GlobalFunction
   def urlencode(str: String): String = {
-    URLEncoder.encode(str, "UTF-8")
+    val sb = new StringBuilder
+
+    str.foreach {
+      case ' ' =>
+        sb.append('+')
+      case ch if (ch < '0' && ch != '-' && ch != '.') ||
+        (ch < 'A' && ch > '9') ||
+        (ch > 'Z' && ch < 'a' && ch != '_') ||
+        (ch > 'z') =>
+        val code = ch.toInt
+        sb.append('%')
+        sb.append(hexdigits((code >> 4) & 0xf))
+        sb.append(hexdigits((code) & 0xf))
+      case ch =>
+        sb.append(ch)
+    }
+    sb.toString()
+  }
+
+
+  @GlobalFunction
+  def rawurlencode(str: String): String = {
+    val sb = new StringBuilder
+
+    str.foreach {
+      case ch if (ch < '0' && ch != '-' && ch != '.') ||
+        (ch < 'A' && ch > '9') ||
+        (ch > 'Z' && ch < 'a' && ch != '_') ||
+        (ch > 'z' && ch != '~') =>
+        val code = ch.toInt
+        sb.append('%')
+        sb.append(hexdigits((code >> 4) & 0xf))
+        sb.append(hexdigits((code) & 0xf))
+      case ch =>
+        sb.append(ch)
+    }
+    sb.toString()
+  }
+
+  @GlobalFunction
+  def stripslashes(str: String): String = {
+    val sb = new StringBuilder
+    var idx = 0
+    while (idx < str.length) {
+      str.charAt(idx) match {
+        case '\\' =>
+          idx += 1
+          str.charAt(idx) match {
+            case '0' =>
+              sb.append('\0')
+            case ch =>
+              sb.append(ch)
+          }
+        case ch =>
+          sb.append(ch)
+      }
+      idx += 1
+    }
+    sb.toString()
+  }
+
+  @GlobalFunction
+  def urldecode(str: String): String = {
+    URLDecoder.decode(str, "UTF-8")
+  }
+
+  @GlobalFunction
+  def rawurldecode(str: String): String = {
+    URLDecoder.decode(str, "UTF-8")
+  }
+
+  @GlobalFunction
+  def uniqid(optPrefix: Option[String], optMoreEntropy: Option[Boolean]): String = {
+    val now = System.currentTimeMillis()
+    val sec = now / 1000
+    val usec = now & 0xfffff
+
+    if (optMoreEntropy.getOrElse(false)) {
+      val random = new SecureRandom()
+      "%s%08x%05x%.8f".format(optPrefix.getOrElse(""), sec, usec, random.nextFloat() * 10)
+    } else {
+      "%s%08x%05x".format(optPrefix.getOrElse(""), sec, usec)
+    }
   }
 
   private def replaceWithCount(str: String, target: String, replacement: String): (String, Int) = {
